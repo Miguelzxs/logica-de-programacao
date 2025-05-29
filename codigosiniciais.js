2e2e76f05dbef51ec94b3d0305f1fea7d60d9cd8
miguel <miguelrarquino11@gmail.com>
2025-05-29 10:54:22 -0300

    codigosiniciais

---
+++ codigosiniciais.js
@@ -0,0 +1,82 @@
+let nome = ("miguel")
+console.log(nome)
+
+let idade = "15"
+console.log(idade)
+
+let aprendendoJavaScript = ("true")
+console.log(aprendendoJavaScript)
+
+let sobrenome = ("ricardo")
+let nomecompleto = (nome) + (sobrenome)
+console.log(nomecompleto)
+
+let numero1 = 2
+let numero2 = 5
+let soma = numero1 + numero2
+console.log(soma)
+
+let numero3 = 3
+let numero4 = 8
+let multiplicação = numero3 * numero4
+console.log(multiplicação)
+
+let cidade = ("São Paulo")
+let habitantes = 11000000
+console.log(cidade)
+console.log(habitantes)
+
+let preço = 40
+console.log(preço)
+preço = 80
+console.log(preço)
+
+let MeuValor
+console.log(MeuValor)
+
+let meuNulo = ("null")
+console.log(meuNulo)
+
+/////////////////////////////////////////////////////////////////////////////////
+
+
+let nome = prompt("Digite seu nome: ")
+let idade = Number(prompt("Digite a sua idade: "))
+let email = prompt("Digite seu email: ")
+let telefone = Number(prompt("Digite seu telefone: "))
+
+console.log ("cadastro finalizado")
+
+
+let dataDeNascimento = Number(prompt("Digite seu ano de nascimento: "))
+2025 - dataDeNascimento
+console.log ( 2025 - dataDeNascimento)
+
+
+
+/////////////////////////////////////////////////////////////////////////////////
+
+
+let inicio = ("Seja bem vindo ao site sócio avanti torcedor")
+console.log (inicio)
+let cadastro = ("cadastre-se ou faça login")
+console.log (cadastro)
+let nome = prompt("Digite seu nome: ")
+let idade = Number(prompt("Digite a sua idade: "))
+let email = prompt("Digite seu email: ")
+let telefone = Number(prompt("Digite seu telefone: "))
+
+console.log ("cadastro finalizado")
+
+let dataDeNascimento = Number(prompt("Digite seu ano de nascimento: "))
+2025 - dataDeNascimento
+console.log ( 2025 - dataDeNascimento)
+let final = ("cadastro finalizado com sucesso")
+console.log (final)
+
+
+/////////////////////////////////////////////////////////////////////////////////
+
+
+
+
